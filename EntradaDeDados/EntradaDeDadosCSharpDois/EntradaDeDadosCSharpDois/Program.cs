5755f793afb06695b8a82bc0ec6fea0d89a04fba
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-05-08 17:15:19 -0300

    update

---
+++ EntradaDeDados/EntradaDeDadosCSharpDois/EntradaDeDadosCSharpDois/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a Idade:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Sexo:");
+            char ch = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Altura:");
+            double n2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite a Altura:");
+            string[] vet = Console.ReadLine().Split(" ");
+            string Nome = vet[0];
+            char Sexo = char.Parse(vet[1]);
+            int Idade = int.Parse(vet[2]);
+            double Altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Voce Digitou: ");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(Nome);
+            Console.WriteLine(Sexo);
+            Console.WriteLine(Idade);
+            Console.WriteLine(Altura.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ EntradaDeDados/ExerPropEntradaDeDadosUm/ExerPropEntradaDeDadosUm/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerPropEntradaDeDadosUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome");
+            string Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o numero de quartos da sua casa: ");
+            int Quartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preco de um produto: ");
+            double Precproduto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu ultimo nome, idade e altura: (mesma linha) ");
+            string[] Vet = Console.ReadLine().Split(" ");
+            String LastName = Vet[0];
+            int Idade = int.Parse(Vet[1]);
+            double Altura = double.Parse(Vet[2]);
+
+
+
+            Console.WriteLine("Voce Digitou: ");
+            Console.WriteLine(Nome);
+            Console.WriteLine(Quartos);
+            Console.WriteLine(Precproduto.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(LastName);
+            Console.WriteLine(Idade);
+            Console.WriteLine(Altura.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
