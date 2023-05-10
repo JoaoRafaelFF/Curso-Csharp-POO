690dafb83a146d7a211566a04d6fbbc302e2b5c4
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-05-09 23:12:05 -0300

    Operadores e Estruturas

---
+++ Operadores&EstrurasIfElse/EstruturaCondicionalIfElse/EstruturaCondicionalIfElse/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EstruturaCondicionalIfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //simples
+            //int x = 10;
+            //Console.WriteLine("Bom dia!");
+            //if (x > 5)
+            //{
+            //Console.WriteLine("Boa Tarde!");
+            //}
+            //Console.WriteLine("Boa Noite!");
+
+
+            //compsta
+            //.WriteLine("Entre com um numero inteiro! ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x % 2 == 0)
+            // {
+            //Console.WriteLine("numero par!");
+            //}
+            //else
+            //{
+            //Console.WriteLine("numero impar!");
+
+
+            Console.WriteLine("Digite a hora atual!");
+
+            int Hora = int.Parse(Console.ReadLine());
+
+            if (Hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (Hora < 18)
+            {
+                Console.WriteLine("Boa Taede!");
+            }
+            else
+            {
+                Console.WriteLine("Boa Noite!");
+            }
+        }
+
+
+    }
+}
---
+++ Operadores&EstrurasIfElse/OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 2 > 3 && 4 != 5; //false v e f = f
+            bool c2 = 2 > 3 || 4 != 5; // true
+            bool c3 = !(2 > 3) && 4 != 5; // (nao 2>3 = v e 4 != 5 = v) = true 
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+
+            Console.WriteLine("--------------------");
+
+            bool c4 = 10 < 5; // false
+            bool c5 = c2 || c3 && c4; // c2 e c3 = fase || cs true = true
+
+            Console.WriteLine(c4);
+            Console.WriteLine(c5); 
+        }
+    }
+}
