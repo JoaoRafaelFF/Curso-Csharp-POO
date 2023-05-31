6d2cd638990949e69a7bde33014adbc8a1b5b9be
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-05-31 18:07:00 -0300

    Classes

---
+++ Course/ClasseComTresAtributos/ClasseComTresAtributos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using ClasseComTresAtributos;
+
+namespace ClasseComTresAtrubutos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do Triangulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Entre com as medidas do Triangulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double P = (x.A + x.B + x.C) / 2;
+            double AreaX = Math.Sqrt(P * (P - x.A) * (P - x.B) * (P - x.C));
+
+            P = (y.A + y.B + y.C) / 2;
+            double AreaY = Math.Sqrt(P * (P - y.A) * (P - y.B) * (P - y.C));
+
+            Console.WriteLine("Area de X igual a: " + AreaX.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Area de Y igual a: " + AreaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (AreaX > AreaY)
+            {
+                Console.WriteLine("X tem maior area");
+            }
+            else
+            {
+                Console.WriteLine("Y tem maior area");
+            }
+        }
+    }
+}
