f661916b2f77b7e778fdc62ca5a067266b986a4a
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-06-01 21:01:40 -0300

    Ex um e dois

---
+++ ExerciciosComClasses/ExercicioDois/ExercicioDois/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDois
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Digite os dados do primeiro funcionario: ");
+            Console.WriteLine("Nome");
+            f1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o salario do primeiro funcionario: ");
+            f1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os dados do segundo funcionario: ");
+            Console.WriteLine("Nome");
+            f2.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o salario do segundo funcionario: ");
+            f2.Salario = double.Parse(Console.ReadLine());
+
+            double media = (f1.Salario + f2.Salario) / 2.0;
+
+            Console.WriteLine("A media dos salarios de " + f1.Nome + " e " + f2.Nome + " é de: " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ ExerciciosComClasses/ExercicioUm/ExercicioUm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExercicioUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Digite os dados da primeira pessoa: ");
+            Console.WriteLine("Nome :");
+            p1.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os dados da segunda pessoa: ");
+            Console.WriteLine("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha é: " + p1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha é: "+ p2.Nome);
+            }
+        }
+    }
+}
