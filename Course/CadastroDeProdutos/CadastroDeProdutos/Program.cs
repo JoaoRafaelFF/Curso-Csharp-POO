241fd219cb60efffdd0ca5e013659322e22afe55
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-06-02 18:36:40 -0300

    Cadastro de produtos exemplo

---
+++ Course/CadastroDeProdutos/CadastroDeProdutos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroDeProdutos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto!");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantudade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionados ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados do produto: " + p);
+
+            Console.Write("Digite o número de produtos a ser Removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+        }
+    }
+}
