210bfb278906926affc7632418cd73f549d56a9d
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-06-12 17:23:32 -0300

    Consturores Sobrecarga e This

---
+++ Course/ConstrutoresSobrecargaThis/namespace ConstrutoresExemploVercQuatro/namespace ConstrutoresExemploVercQuatro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using ConstrutoresExemploVercDois;
+
+namespace ConstrutoresExemploVercQuatro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nomeaux = Console.ReadLine();
+            Console.Write("Preço: ");
+            double precoaux = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+          
+
+            Produto p = new Produto(nomeaux, precoaux);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
