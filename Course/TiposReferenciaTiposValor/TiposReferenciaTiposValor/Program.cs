d902f75447a02289aa455915042ac97c123bf9b4
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-06-21 11:46:49 -0300

    Tipos Referencia & Valor

---
+++ Course/TiposReferenciaTiposValor/TiposReferenciaTiposValor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TiposReferenciaTiposValor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PointStruct p; // tipo struct nao precia mas aceita o new
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+
+            p= new PointStruct();
+
+            Console.WriteLine(p);
+        }
+    }
+}
