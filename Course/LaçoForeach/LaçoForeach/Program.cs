4ad436245b4d9bfced6714562a9f573f4599396e
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-06-28 22:19:10 -0300

    Foreach

---
+++ Course/LaçoForeach/LaçoForeach/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LaçoForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Bob", "Alex" };
+
+            for (int i = 0; i < vect.Length; i++) 
+            {
+                Console.WriteLine(vect[i]);
+            }
+
+
+            Console.WriteLine("------------------------------------------------------");
+
+
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+        }
+    }
+}
