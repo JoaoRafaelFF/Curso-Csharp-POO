7c1900768bdec9c14f32056384de8092685fa0d8
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-06-28 22:03:41 -0300

    Parâmetros Ref & Out

---
+++ Course/ParametrosRefeOut/ParametrosRefeOut/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ParametrosRefeOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ref
+            //int a = 10;
+            //Calculator.Triple(ref a); // exige variavel iniciada
+            //Console.WriteLine(a);
+
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+
+            // ambos são codigos ruims -evitar-
+        }
+    }
+}
