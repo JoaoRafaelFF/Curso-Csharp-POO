a7825fb8010afdfccb54cf039fcab9650008ac54
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-06-28 21:46:49 -0300

    params

---
+++ Course/ParametroParams/ParametroParams/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ParametroParams;
+
+namespace PrametroParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int s1 = calculator.Sum(2, 3);
+            // int s2 = calculator.Sum(3, 4, 2);
+
+
+            int s1 = calculator.Sum(2, 3); // com params nao precisa instaciae o new int[] {x,y, z...}
+            int s2 = calculator.Sum(3, 4, 2);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+
+        }
+    }
+}
