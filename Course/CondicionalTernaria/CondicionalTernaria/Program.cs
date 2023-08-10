a05fb00740934c6c15d5d33749bae7026f69e765
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2023-08-09 21:17:28 -0300

    condicional ternaria

---
+++ Course/CondicionalTernaria/CondicionalTernaria/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace CondicionalTernaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor deciaml: ");
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+            /*double desconto;
+            if (preco < 20)
+            {
+                desconto = preco * 0.1;
+            }
+            else 
+            {
+                desconto = preco * 0.05;
+            }
+            */
+
+            Console.WriteLine(desconto.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
