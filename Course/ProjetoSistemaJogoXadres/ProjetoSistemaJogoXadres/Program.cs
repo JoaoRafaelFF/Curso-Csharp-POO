0e878c0ad07d6bb76f8c96ad10e030025f7a3234
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2024-02-01 21:32:18 -0300

    projeto jogo de xadrez

---
+++ Course/ProjetoSistemaJogoXadres/ProjetoSistemaJogoXadres/Program.cs
@@ -0,0 +1,14 @@
+﻿using tabuleiro;
+namespace ProjetoSistemaJogoXadres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // testes 
+          Tabuleiro tab = new Tabuleiro(8,8);
+            Console.ReadLine();
+
+        }
+    }
+}
