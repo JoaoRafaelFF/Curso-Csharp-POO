5d03d9680876128de6d9508e215ad91085bf58a6
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2024-02-08 20:56:21 -0300

    update na classe Tela

--- Course/ProjetoSistemaJogoXadres/ProjetoSistemaJogoXadres/Program.cs
+++ Course/ProjetoSistemaJogoXadres/ProjetoSistemaJogoXadres/Program.cs
@@ -6,31 +6,34 @@ namespace ProjetoSistemaJogoXadres
     {
         static void Main(string[] args)
         {
-            // testes 
-            // try
-            //{
-            //  Tabuleiro tab = new Tabuleiro(8, 8);
+            //testes 
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 0)); /*dispara validacao já existe peca se ativo */
-            //tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 9)); /*dispara validacao posicao invalida se ativo */
-            //tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            //tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                //tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 0)); //dispara validacao já existe peca se ativo 
+                //tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 9)); //dispara validacao posicao invalida se ativo 
+                tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPecas(new Rei(tab, Cor.Branca), new Posicao(4, 4));
+                tab.colocarPecas(new Rei(tab, Cor.Branca), new Posicao(6, 6));
 
-            //Tela.imprimirTabuleiro(tab);
-            //}
+                Tela.imprimirTabuleiro(tab);
+            }
 
-            //catch (TabuleiroException e) 
-            //{
-            //Console.WriteLine(e.Message);
-            //}
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
-            PosicaoXadres pos = new PosicaoXadres('c',7);
+           /* 
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
             Console.WriteLine(pos);
 
             Console.WriteLine(pos.toPosicao());
-
+           */
             Console.ReadLine();
 
         }
