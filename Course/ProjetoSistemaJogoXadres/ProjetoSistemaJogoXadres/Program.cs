bf0c258b20c10825a754bf9e646ba18e823b9517
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2024-02-07 21:22:53 -0300

    Criando validações e exceções

--- Course/ProjetoSistemaJogoXadres/ProjetoSistemaJogoXadres/Program.cs
+++ Course/ProjetoSistemaJogoXadres/ProjetoSistemaJogoXadres/Program.cs
@@ -7,14 +7,23 @@ namespace ProjetoSistemaJogoXadres
         static void Main(string[] args)
         {
             // testes 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPecas(new Rei(tab,Cor.Preta), new Posicao(2, 4));
+                tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                //tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 0)); /*dispara validacao já existe peca se ativo */
+                //tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 9)); /*dispara validacao posicao invalida se ativo */
+                tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
 
+            catch (TabuleiroException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
 
         }
