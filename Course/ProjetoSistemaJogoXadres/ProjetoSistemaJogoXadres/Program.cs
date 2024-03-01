c08aa07c24d02fa58cce67e3946d76da85654fd1
JoaoRafaelFF <94474954+JoaoRafaelFF@users.noreply.github.com>
2024-03-01 18:53:31 -0300

    movimento torre e testes iniciais de movimento

--- Course/ProjetoSistemaJogoXadres/ProjetoSistemaJogoXadres/Program.cs
+++ Course/ProjetoSistemaJogoXadres/ProjetoSistemaJogoXadres/Program.cs
@@ -6,35 +6,6 @@ namespace ProjetoSistemaJogoXadres
     {
         static void Main(string[] args)
         {
-            //testes 
-            /* try
-             {
-                 Tabuleiro tab = new Tabuleiro(8, 8);
-
-                 tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                 //tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 0)); //dispara validacao já existe peca se ativo 
-                 //tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 9)); //dispara validacao posicao invalida se ativo 
-                 tab.colocarPecas(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                 tab.colocarPecas(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-                 tab.colocarPecas(new Rei(tab, Cor.Branca), new Posicao(4, 4));
-                 tab.colocarPecas(new Rei(tab, Cor.Branca), new Posicao(6, 6));
-
-                 Tela.imprimirTabuleiro(tab);
-             }
-
-             catch (TabuleiroException e)
-             {
-                 Console.WriteLine(e.Message);
-             }
-            */
-
-            /* 
-             PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-
-             Console.WriteLine(pos);
-
-             Console.WriteLine(pos.toPosicao());
-            */
             try 
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
@@ -45,6 +16,13 @@ namespace ProjetoSistemaJogoXadres
                     Tela.imprimirTabuleiro(partida.tab);
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
